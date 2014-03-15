176a1ec6660e6834ad18c359bfe84e7675f4ab33
Paul Kulyk <paul.kulyk@gmail.com>
2014-03-14 19:41:16 -0600

    Updated some fonts... maybe uglied

--- _site/javascripts/header.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $("#search-icon").click(function() {
-    $("#search form").slideDown(function() {
-      $(this).find("input[type=text]").focus();
-    })
-    return false;
-  });
-});
